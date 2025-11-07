942c8f57ed5e2bff0ffe94ec6865749864f965a5
napapen <napapen.w@gmail.com>
2025-11-07 16:46:30 +0700

    update full text

--- resources/views/forms/full.blade.php
+++ resources/views/forms/full.blade.php
@@ -9,7 +9,7 @@
 
                 <div class="alert alert-warning text-center my-5">
                     <h2>{{ $event }} Registration Closed</h2>
-                    <p>We are sorry, but the registration for <strong>{{ $event }}</strong> has reached its limit of {{ $limit }} participants.</p>
+                    <p>We are sorry, but the registration for <strong>{{ $event }}</strong> has been closed.</p>
                     <p>Please contact our team if you have any questions.</p>
                 </div>
 
